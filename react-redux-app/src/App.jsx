d0954c0847c1a7838be1b2a2aea4391ebdee0253
MdFazilGITHUB <mjam7221jmi@gmail.com>
2024-04-15 09:43:37 +0530

    done

---
+++ react-redux-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import LikeCounter from "./components/LikeCounter";
+
+function App() {
+  return (
+    <>
+      <LikeCounter />
+    </>
+  );
+}
+
+export default App;
